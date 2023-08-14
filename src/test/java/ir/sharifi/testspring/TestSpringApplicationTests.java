404c0cbfe069d4650ce06829754925f456e4b720
hsharifi <hsharifi@fardup.ir>
2023-08-14 18:24:15 +0330

    first commit

---
+++ src/test/java/ir/sharifi/testspring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sharifi.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
